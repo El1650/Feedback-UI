38e130465f8c0965f636c5d017c0f740885ed4d2
Elena G.Mandefro <elamandefro@gmail.com>
2023-12-14 01:12:37 +0530

    Update

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eaque iure  asperiores impedit labore provident consequuntur?",
+  },
+  {
+    id: 2,
+    rating: 6,
+    text: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eaque iure  asperiores impedit labore provident consequuntur?",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eaque iure  asperiores impedit labore provident consequuntur?",
+  },
+];
+
+export default FeedbackData;
